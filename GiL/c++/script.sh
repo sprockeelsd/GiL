1f35bb3427e43e20693de0f09696e7a28f77eaa1
Damien Sprockeels <sprockeelsd@mac-1l0-370.local>
2021-10-15 16:39:12 +0200

    added comments in the script

--- GiL/c++/script.sh
+++ GiL/c++/script.sh
@@ -8,4 +8,6 @@
     cd sources
 
     #update the path
+    #in MacOS 10.15.7, there was a change in the security and the relative path to gecode didn't allow the dynamic library to
+    #correctly locate gecode. This script allows to change the rpath to the full path so gecode can be found. 
     install_name_tool -change gecode.framework/Versions/49/gecode /Library/Frameworks/gecode.framework/Versions/49/gecode libgecode.dylib
